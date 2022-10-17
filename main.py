44edbf0e847efcc9c5152aa4a29e038ea0d057fc
Shashikant sawadh <sawadhofficial3@gmail.com>
2022-10-17 09:46:19 +0530

    this is my first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+     uvicorn.run("main:app", port=8000, reload=True)
+
+
+
